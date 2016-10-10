e148c9d7efdfd03539556f0b7aab51e1b5d7f0dc
Steven <sbow@Stevens-MacBook.local>
2016-10-10 22:08:49 +0100

    Updating the setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
-sh .osx
+#sh .osx
+ln -sfv ~/dotfiles/plugins.vim ~/.vim
+ln -sfv ~/dotfiles/snippets ~/.vim
+ln -sfv ~/dotfiles/railscast.vim ~/.vim/colors
+ln -sfv ~/dotfiles/.vimrc ~/
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install ctags
+brew install wget
+brew install the_silver_surfer
+brew install php-cs-fixer
